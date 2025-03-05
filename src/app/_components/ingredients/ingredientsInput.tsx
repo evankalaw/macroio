cc59d0dd7f2d8dfb8ff9f4460545c4e816a9be65
Evan Kalaw <evankalaw@Evans-MacBook-Pro.local>
2025-03-04 19:34:15 -0600

    handle basic input functionality

--- src/app/_components/ingredients/ingredientsInput.tsx
+++ src/app/_components/ingredients/ingredientsInput.tsx
@@ -1,3 +1,25 @@
+import { useState } from "react";
+
 export default function IngredientsInput() {
-  return <input type="text" placeholder="greek yogurt" />;
+  const [inputText, setInputText] = useState<string>("");
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInputText(e.target.value);
+  };
+
+  const handleInputSubmit = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") {
+      console.log(inputText);
+    }
+  };
+
+  return (
+    <input
+      type="text"
+      className="text-black"
+      placeholder="greek yogurt"
+      onChange={handleInputChange}
+      onKeyDown={handleInputSubmit}
+    />
+  );
 }
